86bcedd47ab1cc3370235f5a37bed7e19008cfe2
Honey Sharma <sharmahoney3039@gmail.com>
2026-02-09 23:18:44 +0530

    Fix connectDB export

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,17 +1,13 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 
-dotenv.config();
-
-exports.connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }
-    catch (error) {
-        console.log("Error while connecting to MongoDB", error);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("Error while connecting to MongoDB", error);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDB;
+module.exports = connectDB; 
